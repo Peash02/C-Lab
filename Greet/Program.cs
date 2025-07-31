2d2747d21d6d1ecac85b82f7d286ebdf390d13df
Peash <107058247+Peash02@users.noreply.github.com>
2025-07-31 14:48:19 +0530

    Add files via upload

---
+++ Greet/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Greet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name:");
+            String name = Console.ReadLine();
+            PP p = new PP(name);
+            p.greet();
+        }
+    }
+}
