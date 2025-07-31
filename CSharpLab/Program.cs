ff31c7d0c102db82b4f7544c8211a61e3bf78440
Peash <107058247+Peash02@users.noreply.github.com>
2025-07-31 14:52:22 +0530

    Added Square

---
+++ CSharpLab/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sqr s = new Sqr();
+            s.calc();
+            s.display();
+        }
+    }
+}
---
+++ CSharpLab/Sqr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    public class Sqr
+    {
+        int num;
+        int sqr;
+        public Sqr()
+        {
+            num = 2;
+        }
+        public Sqr(int num)
+        {
+            this.num = num;
+        }
+        public void calc()
+        {
+            sqr = num * num;
+        }
+        public void display()
+        {
+            Console.WriteLine("The Square Of " + num + " is " +  sqr);
+        }
+    }
+}
