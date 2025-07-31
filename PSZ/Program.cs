898cdce57e84dce2dfde1147f21b045c23392092
Peash <107058247+Peash02@users.noreply.github.com>
2025-07-31 14:49:21 +0530

    Add files via upload

---
+++ PSZ/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PSZ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            pnz n = new pnz(num);
+            n.check();
+        }
+    }
+}
---
+++ PSZ/pnz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSZ
+{
+    public class pnz
+    {
+        int num;
+
+        public pnz()
+        {
+            num = 2;
+        }
+        public pnz(int num)
+        {
+            this.num = num;
+        }
+        public void check()
+        {
+            if (num ==0)
+            {
+                Console.WriteLine("The Number is Zero.");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("The Number is Positive.");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Negative.");
+            }
+        }
+    }
+}
