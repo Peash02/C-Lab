a81ae922346ea1269e5c748c0166228d095c46a5
Peash <107058247+Peash02@users.noreply.github.com>
2025-08-07 14:57:17 +0530

    Add files via upload

---
+++ LabPrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataEncapsulation;
+
+namespace LabPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            Console.WriteLine("Enter Persons Name:");
+            person1.Name = Console.ReadLine();
+            Console.WriteLine($"Enter the Age of {person1.Name}:");
+            person1.Age = int.Parse(Console.ReadLine());
+            
+            //person1.Name = "Alice";
+            //person1.Age = 20;
+
+            Console.WriteLine($"The Name is {person1.Name} and Age is {person1.Age}.");
+        }
+    }
+}
