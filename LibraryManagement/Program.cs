15e6a4f289c89b5a0860c9bb81238bcd8635b163
Peash <107058247+Peash02@users.noreply.github.com>
2025-08-14 14:53:19 +0530

    Add files via upload

---
+++ LibraryManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book("Harry Potter", "J.K. Rowling");
+            b.DisplayDetails();
+            Book e = new EBook("Hunger Games", "Somebody", 69, "EPUB");
+            e.DisplayDetails();
+        }
+    }
+}
