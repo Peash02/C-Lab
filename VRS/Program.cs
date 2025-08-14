0ae15e8dacdfcc02915deb2b12326667cf490c71
Peash <107058247+Peash02@users.noreply.github.com>
2025-08-14 15:07:20 +0530

    Add files via upload

---
+++ VRS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VRS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle("Gixxer", "Suzuki", 69);
+            v.display();
+            Vehicle c = new Car("City", "Honda", 699,5,"Petrol");
+            c.display();
+        }
+    }
+}
