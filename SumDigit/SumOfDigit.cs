32a7e93d3132f488e2753f6cfc5eb4a0757a9a67
Peash <107058247+Peash02@users.noreply.github.com>
2025-10-23 14:43:10 +0530

    Add files via upload

---
+++ SumDigit/SumOfDigit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    internal class SumOfDigit
+    {
+        int num;
+        int sum = 0;
+        int temp;
+        public SumOfDigit(int num)
+        {
+            this.num = num;
+        }
+        public void process()
+        {
+            while (num > 0)
+            {
+                temp = num % 10;
+                num /= 10;
+                sum = sum + temp;
+            }
+
+        }
+        public void display()
+        {
+            Console.WriteLine($"The Sum is {sum}");
+        }
+    }
+}
